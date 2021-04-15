0d62f39a761d8aaf147feef35849f71f26c25b5e
mohammad javad <mohammadjavad.mirshekari@gmail.com>
2021-04-15 16:08:59 +0430

    Remove pyramid layers and make it sutable for 3 variables

---
+++ retinanet/anchor_utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def generate_anchors(angle_split, num_variables):
+    """
+    Generate (reference) anchors 
+    """
+    num_anchors = angle_split
+
+    # initialize output anchors
+    anchors = np.zeros((num_anchors, num_variables))
+
+    b = (360 / angle_split)
+    anchors[:, -1] = [0+b*n for n in range(angle_split)]
+
+    return anchors
+
+
+def anchors_for_shape(
+    image_shape,
+    angle_split=None,
+    num_variables=None,
+    stride=None
+):
+
+    all_anchors = np.zeros((0, num_variables))
+    anchors = generate_anchors(
+        angle_split=angle_split, num_variables=num_variables)
+    shifted_anchors = shift(image_shape, stride, anchors)
+    all_anchors = np.append(all_anchors, shifted_anchors, axis=0)
+
+    return all_anchors
+
+
+def shift(shape, stride, anchors):
+    """ Produce shifted anchors based on shape of the map and stride size.
+
+    Args
+        shape  : Shape to shift the anchors over.
+        stride : Stride to shift the anchors with over the shape.
+        anchors: The anchors to apply at each location.
+    """
+    # create a grid starting from half stride from the top left corner
+    shift_x = (np.arange(0, shape[1]) + 0.5) * stride
+    shift_y = (np.arange(0, shape[0]) + 0.5) * stride
+
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+
+    shifts = np.vstack((
+        shift_x.ravel(), shift_y.ravel(),
+        np.zeros_like(shift_y).ravel()
+    )).transpose()
+
+    # add A anchors (1, A, 3) to
+    # cell K shifts (K, 1, 3) to get
+    # shift anchors (K, A, 3)
+    # reshape to (K*A, 3) shifted anchors
+    A = anchors.shape[0]
+    K = shifts.shape[0]
+    all_anchors = (anchors.reshape((1, A, 3)) +
+                   shifts.reshape((1, K, 3)).transpose((1, 0, 2)))
+    all_anchors = all_anchors.reshape((K * A, 3))
+
+    return all_anchors
