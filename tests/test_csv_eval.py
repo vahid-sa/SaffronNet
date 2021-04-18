9ea7f1e7da8db61896f7b8d6042170e635801a80
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-04-18 22:39:54 +0430

    Write 2 test for csv_eval

--- tests/test_csv_eval.py
+++ tests/test_csv_eval.py
@@ -1,8 +1,39 @@
 import unittest
 import numpy as np
-from retinanet.anchor_utils import generate_anchors
+from retinanet.csv_eval import compute_distance, __prepare as prepare
+
 
 class TestCSVEval(unittest.TestCase):
     """ Test Anchor's functions functionality
     """
-    pass
+
+    def test_prepare(self):
+        data = {
+            'a': np.array([2.1]),  # a: (N)
+            'b': np.array([1.1, 1.2, 1.3, 1.4])  # b: (k)
+        }
+
+        a, b = prepare(data['a'], data['b'])  # prepare: (N, k)
+        assert a.shape == b.shape
+        assert a.shape[0] == data['a'].shape[0]
+        assert a.shape[1] == data['b'].shape[0]
+
+    def test_compute_distance(self):
+        """ test compute distance
+        """
+        data = {
+            'a': np.array([[2.1, 2.2, 23]]),  # a: (N)
+            # b: (k)
+            'b': np.array([[3.1, 3.2, 33], [4.1, 4.2, 22], [5.1, 5.2, 53]])
+        }
+        dxys, dangles = compute_distance(data['a'], data['b'])
+
+        assert dxys.shape == dangles.shape
+
+        assigned_annotation = np.argmin(dxys, axis=1)
+        min_dxy = dxys[0, assigned_annotation]
+        assert assigned_annotation == [0]
+
+        assigned_annotation = np.argmin(dangles, axis=1)
+        min_dangel = dangles[0, assigned_annotation]
+        assert assigned_annotation == [1]
