5c572dda1ad71efe3e7f7d07574950062153005b
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-05-04 18:48:10 +0430

    Bugfix on indents

--- retinanet/dataloader.py
+++ retinanet/dataloader.py
@@ -413,7 +413,7 @@ class Normalizer(object):
     """ Normalizer: checked!
     """
 
-   def __init__(self):
+    def __init__(self):
         self.mean = np.array([[[0.485, 0.456, 0.406]]])
         self.std = np.array([[[0.229, 0.224, 0.225]]])
 
