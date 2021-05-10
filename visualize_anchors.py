428293a191da748d1f4e6eacd8290d313213627f
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-05-10 17:09:15 +0430

    Test-51

--- visualize_anchors.py
+++ visualize_anchors.py
@@ -1,6 +1,7 @@
 from retinanet.dataloader import CocoDataset, CSVDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, \
     UnNormalizer, Normalizer
 
+from retinanet.anchors import Anchors
 from utils.visutils import write_angle, draw_line
 import matplotlib.pyplot as plt
 from torchvision import datasets, models, transforms
@@ -56,21 +57,29 @@ def main(args=None):
         raise ValueError(
             'Dataset type not understood (must be csv or coco), exiting.')
 
-    for i in range(len(dataset)):
-        image = (dataset.load_image(i) * 255).astype(np.int32)
-        anots = dataset.load_annotations(i)
-        for anot in anots:
-            x, y, alpha = anot[0], anot[1], 90 - anot[2]
-            image = draw_line(
-                image, (x, y), alpha,
-                line_color=(0, 0, 0),
-                center_color=(255, 0, 0),
-                half_line=True
-            )
+    # for i in range(len(dataset)):
+    #     image = (dataset.load_image(i) * 255).astype(np.int32)
+    #     anots = dataset.load_annotations(i)
+    #     for anot in anots:
+    #         x, y, alpha = anot[0], anot[1], 90 - anot[2]
+    #         image = draw_line(
+    #             image, (x, y), alpha,
+    #             line_color=(0, 0, 0),
+    #             center_color=(255, 0, 0),
+    #             half_line=True
+    #         )
 
-        image_name = os.path.basename(dataset.image_names[i])
-        cv.imwrite(os.path.join(parser.save_dir, image_name),
-                   cv.cvtColor(image, cv.COLOR_RGB2BGR))
+    #     image_name = os.path.basename(dataset.image_names[i])
+    #     cv.imwrite(os.path.join(parser.save_dir, image_name),
+    #                cv.cvtColor(image, cv.COLOR_RGB2BGR))
+
+    sample_image = (dataset.load_image(0) * 255).astype(np.int32)
+    sample_batch = np.expand_dims(sample_image, axis=0)
+    sample_batch = sample_batch.permute(0, 3, 1, 2)
+    print(sample_batch)
+    anchros_mudole = Anchors()
+    anchors = anchros_mudole(sample_batch)
+    print(anchors)
 
 
 if __name__ == '__main__':
