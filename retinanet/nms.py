6012c22d3005442d8112428ef4663e3706e4fe10
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-05-11 15:17:43 +0430

    Test-71

---
+++ retinanet/nms.py
@@ -0,0 +1,22 @@
+import numpy as np
+from losses import __prepare
+
+
+def nms(predictions, scores, min_score):
+    """ Apply nms over predictions
+        inputs: 
+            predictions: torch.Tensor
+            scores: torch.Tensor
+            min_scores: int
+        return:
+            anchors_nms_idx: np.ndarray
+    """
+    print(predictions.type)
+    print(scores.type)
+    # r_score, c_score1 = __prepare(scores)
+    # scores = r_score - c_score1
+
+    # best = []
+
+    # valid_scores_indices = scores > min_score
+    # for i in range(predictions.shape[0]):
