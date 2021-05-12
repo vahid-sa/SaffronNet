71843eaa522f12de268ac23b9a5f40010f79c722
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-05-12 19:02:50 +0430

    Amend 'visualize_single_image'

--- visualize_single_image.py
+++ visualize_single_image.py
@@ -25,6 +25,18 @@ def load_classes(csv_reader):
     return result
 
 
+def draw_line(img, bbox):
+    half_length = 10
+    x, y, alpha = bbox
+    rows, cols, ch = img.shape
+    xx, yy = int(round(half_length * np.cos(alpha))), int(round(half_length * np.sin(alpha)))
+    start_point = max(x - xx, 0), max(y - yy, 0)
+    end_point = min(x + xx, cols - 1), min(y + yy, rows - 1)
+    print(start_point, end_point)
+    cv2.line(img=img, pt1=start_point, pt2=end_point, color=(0, 0, 255), thickness=3)
+
+
+
 # Draws a caption above the box in an image
 def draw_caption(image, box, caption):
     b = np.array(box).astype(int)
@@ -58,36 +70,15 @@ def detect_image(image_path, model_path, class_list):
 
         rows, cols, cns = image.shape
 
-        smallest_side = min(rows, cols)
-
-        # rescale the image so the smallest side is min_side
-        min_side = 608
-        max_side = 1024
-        scale = min_side / smallest_side
-
-        # check if the largest side is now greater than max_side, which can happen
-        # when images have a large aspect ratio
-        largest_side = max(rows, cols)
-
-        if largest_side * scale > max_side:
-            scale = max_side / largest_side
-
-        # resize the image with the computed scale
-        image = cv2.resize(image, (int(round(cols * scale)), int(round((rows * scale)))))
-        rows, cols, cns = image.shape
-
         pad_w = 32 - rows % 32
         pad_h = 32 - cols % 32
 
         new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
         new_image[:rows, :cols, :] = image.astype(np.float32)
         image = new_image.astype(np.float32)
-        image /= 255
-        image -= [0.485, 0.456, 0.406]
-        image /= [0.229, 0.224, 0.225]
+
         image = np.expand_dims(image, 0)
         image = np.transpose(image, (0, 3, 1, 2))
-
         with torch.no_grad():
 
             image = torch.from_numpy(image)
@@ -95,7 +86,8 @@ def detect_image(image_path, model_path, class_list):
                 image = image.cuda()
 
             st = time.time()
-            print(image.shape, image_orig.shape, scale)
+
+            print(image.shape)
             scores, classification, transformed_anchors = model(image.cuda().float())
             print('Elapsed time: {}'.format(time.time() - st))
             idxs = np.where(scores.cpu() > 0.5)
@@ -103,20 +95,21 @@ def detect_image(image_path, model_path, class_list):
             for j in range(idxs[0].shape[0]):
                 bbox = transformed_anchors[idxs[0][j], :]
 
-                x1 = int(bbox[0] / scale)
-                y1 = int(bbox[1] / scale)
-                x2 = int(bbox[2] / scale)
-                y2 = int(bbox[3] / scale)
+                x, y, alpha = int(bbox[0]), int(bbox[1]), int(bbox[2])
                 label_name = labels[int(classification[idxs[0][j]])]
                 print(bbox, classification.shape)
                 score = scores[j]
                 caption = '{} {:.3f}'.format(label_name, score)
                 # draw_caption(img, (x1, y1, x2, y2), label_name)
-                draw_caption(image_orig, (x1, y1, x2, y2), caption)
-                cv2.rectangle(image_orig, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+                draw_caption(image_orig, (x, y, alpha), caption)
+                # cv2.rectangle(image_orig, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+                cv2.circle(image, (x, y), 5, color=(0, 0, 255), thickness=-1)
+                draw_line(img=image, bbox=bbox)
 
             cv2.imshow('detections', image_orig)
             cv2.waitKey(0)
+
+
 
 
 if __name__ == '__main__':
