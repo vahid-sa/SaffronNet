057f8447f6ebbf6d0745e2d7f4439456a4614b01
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-05-16 18:07:12 +0430

    ignore to calculate mAp

--- train.py
+++ train.py
@@ -181,17 +181,17 @@ def main(args=None):
                 print(e)
                 continue
 
-        # if parser.dataset == 'coco':
+        if parser.dataset == 'coco':
 
-        #     print('Evaluating dataset')
+            print('Evaluating dataset')
 
-        #     coco_eval.evaluate_coco(dataset_val, retinanet)
+            coco_eval.evaluate_coco(dataset_val, retinanet)
 
-        # elif parser.dataset == 'csv' and parser.csv_val is not None:
+        elif parser.dataset == 'csv' and parser.csv_val is not None:
 
-        #     print('Evaluating dataset')
+            print('Evaluating dataset')
 
-        #     mAP = csv_eval.evaluate(dataset_val, retinanet)
+            mAP = csv_eval.evaluate(dataset_val, retinanet)
 
         scheduler.step(np.mean(epoch_loss))
 
