15bfe8bdf69182d0064d2e6f116c087959dd0813
vahid-sa <vahid.saeedi7495@gmail.com>
2021-05-24 21:00:44 +0430

    completed

--- predict_boxes.py
+++ predict_boxes.py
@@ -3,6 +3,7 @@
 # dtype: np.float32
 
 import torch
+import torchvision
 import cv2
 import numpy as np
 import csv
@@ -11,85 +12,11 @@ from os import path as osp
 import argparse
 import json
 from retinanet.settings import X, Y, ALPHA, LABEL, SCORE, TRUTH
+from retinanet.utils import load_classes
+from retinanet import imageloader
+from detector import get_detections
+from visualize import draw
 
-
-def load_classes(csv_reader):
-    result = {}
-
-    for line, row in enumerate(csv_reader):
-        line += 1
-
-        try:
-            class_name, class_id = row
-        except ValueError:
-            raise(ValueError('line {}: format should be \'class_name,class_id\''.format(line)))
-        class_id = int(class_id)
-
-        if class_name in result:
-            raise ValueError('line {}: duplicate class name: \'{}\''.format(line, class_name))
-        result[class_name] = class_id
-    return result
-
-
-def detect_image(image_dir, filenames, model_path, class_list, ext=".jpg"):
-
-    with open(class_list, 'r') as f:
-        classes = load_classes(csv.reader(f, delimiter=','))
-    labels = {}
-    for key, value in classes.items():
-        labels[value] = key
-    print("labels", labels)
-    model = torch.load(model_path)
-
-    if torch.cuda.is_available():
-        model = model.cuda()
-
-    model.training = False
-    model.eval()
-    bboxes = list()
-    for img_name in filenames:
-
-        image = cv2.imread(osp.join(image_dir, img_name+ext))
-        if image is None:
-            continue
-        image_orig = image.copy()
-
-        rows, cols, cns = image.shape
-
-        pad_w = 32 - rows % 32
-        pad_h = 32 - cols % 32
-
-        new_image = np.zeros((rows + pad_w, cols + pad_h, cns)).astype(np.float32)
-        new_image[:rows, :cols, :] = image.astype(np.float32)
-        image = new_image.astype(np.float32)
-        image /= 255
-        image -= [0.485, 0.456, 0.406]
-        image /= [0.229, 0.224, 0.225]
-        image = np.expand_dims(image, 0)
-        image = np.transpose(image, (0, 3, 1, 2))
-        with torch.no_grad():
-
-            image = torch.from_numpy(image)
-            if torch.cuda.is_available():
-                image = image.cuda()
-
-            st = time.time()
-
-            scores, classification, transformed_anchors = model(image.cuda().float())
-            print('Elapsed time: {}'.format(time.time() - st))
-        scores = transformed_anchors.cpu().detach().numpy()
-        classification = classification.cpu().detach().numpy()
-        transformed_anchors = transformed_anchors.cpu().detach().numpy()
-        # print("scores: {0}   class: {1}   anchors: {2}".format(scores.shape, classification.shape, transformed_anchors.shape))
-        # print("scores: {0}   class: {1}   anchors: {2}".format(scores.dtype, classification.dtype, transformed_anchors.dtype))
-        # bboxes.extend(np.concatenate((transformed_anchors, classification, scores), axis=1))
-    return bboxes
-
-
-# def sort_by_scores(bboxes):
-#     sorted_indices = bboxes[:, SCORE].argsort()
-#     sorted_bboxes = bboxes[sorted_indices]
-#     return sorted_bboxes
 
 def select_uncertain_indices(boxes, budget, center_score=0.5):
     scores = np.abs(boxes[:, SCORE] - center_score)
@@ -113,20 +40,52 @@ def split(boxes, budget=100):
     noisy_indices = select_noisy_indices(boxes, uncertain_indices)
     uncertain_boxes = boxes[uncertain_indices]
     noisy_boxes = boxes[noisy_indices]
-    return uncertain_boxes, noisy_boxes
+    status = np.full(shape=(len(boxes), 1), dtype=np.float64, fill_value=-1)
+    status[uncertain_indices] = 0
+    status[noisy_indices] = 1
+    # return uncertain_boxes, noisy_boxes
+    return np.concatenate((boxes, status), axis=1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='predicting boxes and split uncertain and noisy')
-    parser.add_argument("--image_dir", help="where images are saved")
-    parser.add_argument("--model_path", help="path to weights")
+    parser.add_argument(
+        "--filenames_path", help="filenames")
+    parser.add_argument(
+        "--partition", help="supervised | unsupervised | validation | test")
+    parser.add_argument(
+        '--image_dir', help='Path to directory containing images')
+    parser.add_argument(
+        '--model_path', help='Path to model')
+    parser.add_argument(
+        '--class_list', help='Path to CSV file listing class names (see README)')
+    parser.add_argument(
+        "--ext", help="image extrension", default=".jpg")
+    parser.add_argument("--output_dir", help="where to save")
     parser = parser.parse_args()
-    image_dir = parser.image_dir
-    model_path = parser.model_path
-    with open("annotations/filenames.json", "r") as fileIO:
-        str_names = fileIO.read()
-    filenames = json.loads(str_names)
-    bboxes = detect_image(image_dir=image_dir, filenames=filenames["unsupervised"], model_path=model_path, class_list="annotations/labels.csv")
-    print(bboxes)
-    print(bboxes.shape)
-    print(bboxes.dtype)
+
+    assert parser.partition in "supervised | unsupervised | validation | test"
+    loader = imageloader.CSVDataset(
+        filenames_path=parser.filenames_path,
+        partition=parser.partition,
+        class_list=parser.class_list,
+        images_dir=parser.image_dir,
+        image_extension=parser.ext,
+        transform=torchvision.transforms.Compose([imageloader.Normalizer(), imageloader.Resizer()]),
+    )
+    retinanet = torch.load(parser.model_path)
+    if torch.cuda.is_available():
+        retinanet = retinanet.cuda()
+
+    retinanet.training = False
+    retinanet.eval()
+    retinanet.module.freeze_bn()
+
+    bboxes = get_detections(loader, retinanet)
+    # uncertain_boxes, noisy_boxes = split(bboxes)
+    bboxes = split(bboxes)
+    # print("uncertain: shape: {0}, dtype: {1}, min_score: {2}, max_score: {3}".format(
+    #     uncertain_boxes.shape, uncertain_boxes.dtype, round(uncertain_boxes[:, SCORE].min(), 2), round(uncertain_boxes[:, SCORE].max(), 2)))
+    # print("noisy: shape: {0}, dtype: {1}, min_score: {2}, max_score: {3}".format(
+    #     noisy_boxes.shape, noisy_boxes.dtype, round(noisy_boxes[:, SCORE].min(), 2), round(noisy_boxes[:, SCORE].max(), 2)))
+    draw(loader=loader, detections=bboxes, images_dir=parser.image_dir, output_dir=parser.output_dir)
