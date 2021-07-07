59d4b9243a760e8c2ce99b69cf90ed36a2a143ef
vahid-sa <vahid.saeedi7495@gmail.com>
2021-07-07 13:14:29 +0430

    Some modification in plots/plot_history.py

--- plots/plot_history.py
+++ plots/plot_history.py
@@ -7,7 +7,7 @@ def plot_loss_mAP(json_file_path: str):
     fileIO = open(json_file_path, "r")
     rows = fileIO.readlines()
     fileIO.close()
-    epochs = list()
+    # epochs = list()
     class_losses = list()
     xyreg_losses = list()
     alphareg_losses = list()
@@ -15,7 +15,7 @@ def plot_loss_mAP(json_file_path: str):
     num_annots = list()
     for row in rows:
         info = json.loads(row)
-        epochs.append(int(info["epoch"]))
+        # epochs.append(int(info["epoch"]))
         class_losses.append(float(info["c-loss"]))
         xyreg_losses.append(float(info["rxy-loss"]))
         alphareg_losses.append(float(info["ra-loss"]))
@@ -24,6 +24,7 @@ def plot_loss_mAP(json_file_path: str):
         d = float(d[0]), int(float(d[1]))
         mAP.append(d[0])
         num_annots.append(d[1])
+    epochs = list(range(len(rows)))
     fig, ax = plt.subplots(2, 2, figsize=(12, 4))
     ax[0, 0].plot(epochs, class_losses, color="red")
     ax[0, 0].set_ylabel("classification loss")
@@ -40,11 +41,11 @@ def plot_loss_mAP(json_file_path: str):
     ax[1, 1].plot(epochs, mAP, color="blue")
     ax[1, 1].set_ylabel("mAP")
     ax[1, 1].set_xlabel("epoch")
-    ax[1, 1].set_ylim([0.0 - 0.05, 1.0 + 0.05])
+    # ax[1, 1].set_ylim([0.0 - 0.05, 1.0 + 0.05])
 
-    fig.suptitle("Number of Annotated Saffrons = {0}".format(num_annots[0]))
+    fig.suptitle("Number of Annotated Saffrons = {0}\nsupervised".format(num_annots[0]))
     plt.show()
 
 
-PATH = '/media/mj-haghighi/repos/pytorch-retinanet/history.json'
+PATH = './history.json'
 plot_loss_mAP(json_file_path=PATH)
