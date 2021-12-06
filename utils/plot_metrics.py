f6b075c4547d5e08e6fb45b39026fe754de5c3cf
Vahid <vahid.saeedi7495@gmail.com>
2021-12-06 15:40:05 +0330

    mAP_train removed from plot

--- utils/plot_metrics.py
+++ utils/plot_metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 from os import path as osp
 from matplotlib import pyplot as plt
 
-path = "/run/user/1000/gvfs/smb-share:server=172.17.3.145,share=saeedi97/tmp/saffron_imgs/metrics.json"
+path = "/run/user/1000/gvfs/smb-share:server=172.17.3.145,share=saeedi97/Safffron/metrics.json"
 
 f = open(path, "r")
 string = f.read()
@@ -13,6 +13,7 @@ metrics = json.loads(string)
 maps = metrics['mAP']
 losses = metrics["loss"]
 lrs = metrics['lr']
+# map_train = metrics['mAP_train']
 cycles = np.unique(metrics['cycle'])
 epochs = np.unique(metrics['epoch'])
 cycles = cycles - 1 if min(cycles) > 0 else cycles
@@ -22,10 +23,14 @@ axs[0].title.set_text("mAP")
 axs[1].plot(np.arange(len(losses)), losses, color='red')
 axs[1].title.set_text("loss")
 axs[2].plot(np.arange(len(lrs)), lrs, color='blue')
+axs[2].title.set_text("learning rate")
+# axs[3].plot(np.arange(len(map_train)), map_train)
+# axs[3].title.set_text("mAP train")
 for cyl in cycles:
     axs[0].axvline(cyl * len(epochs))
     axs[1].axvline(cyl * len(epochs))
     axs[2].axvline(cyl * len(epochs))
+    # axs[3].axvline(cyl * len(epochs))
 fig.suptitle("metrics")
 fig.show()
 plt.show()
