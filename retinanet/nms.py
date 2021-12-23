cc3ab6fcebb6c173bd60669e44100bec1f7df08e
Vahid <vahid.saeedi7495@gmail.com>
2021-12-23 16:37:35 +0330

    remove del and garbage collection from nms

--- retinanet/nms.py
+++ retinanet/nms.py
@@ -1,9 +1,6 @@
 import numpy as np
-from torch._C import dtype
-from .losses import prepare, distance
-import torch as t
-import gc
-from .settings import MAX_ANOT_ANCHOR_ANGLE_DISTANCE, MAX_ANOT_ANCHOR_POSITION_DISTANCE
+from .utils import distance
+import torch
 
 
 def filter(predictions, scores, min_score=0.5):
@@ -26,26 +23,18 @@ def nms(predictions, scores, min_score=0.5, max_distance=20):
     scores = scores[scores_over_thresh]
     predictions = predictions[scores_over_thresh]
 
-    x = predictions[:, 0].cpu()
-    gc.collect()
-    dx = distance(ax=x, bx=x, large_matrix=True)
-    del x
-    y = predictions[:, 1].cpu()
-    gc.collect()
-    dy = distance(ax=y, bx=y, large_matrix=True)
-    del y
-    gc.collect()
-    dxy = t.sqrt(dx*dx + dy*dy)
-    del dx, dy
-    gc.collect()
+    x, y = predictions[:, 0], predictions[:, 1]
+    dx = distance(ax=x, bx=x)
+    dy = distance(ax=y, bx=y)
+    dxy = torch.sqrt(dx*dx + dy*dy)
     for i in range(dxy.shape[0]):
-        filter_row = t.logical_and(-0.01 < dxy[i, :], dxy[i, :] < max_distance)
+        filter_row = torch.logical_and(-0.01 < dxy[i, :], dxy[i, :] < max_distance)
         filter_row = filter_row.nonzero(as_tuple=True)[0]
         candidate_scores = scores[filter_row]
         if candidate_scores.shape[0] == 0:
             continue
-        arg_max = t.argmax(candidate_scores)
-        filter_row = t.cat([filter_row[:arg_max], filter_row[arg_max+1:]])
+        arg_max = torch.argmax(candidate_scores)
+        filter_row = torch.cat([filter_row[:arg_max], filter_row[arg_max+1:]])
         dxy[:, filter_row] = -1
     try:
         valid_indices = (dxy[0, :] > 0).nonzero(as_tuple=True)[0]
